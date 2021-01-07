a9725018683ec282485f21ac104248e536ff0bf5
Eliud Berlin <info@eliud.io>
2021-01-07 21:24:39 +0100

    Membership

--- lib/extensions/widgets/membership_dialog.dart
+++ lib/extensions/widgets/membership_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:eliud_core/core/widgets/progress_indicator.dart';
 import 'package:eliud_pkg_notifications/platform/platform.dart';
-import 'package:eliud_core/model/abstract_repository_singleton.dart';
 import 'package:eliud_core/model/access_model.dart';
 import 'package:eliud_core/tools/common_tools.dart';
 import 'package:eliud_core/tools/widgets/dialog_helper.dart';
@@ -8,7 +7,7 @@ import 'package:eliud_pkg_membership/model/member_public_info_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:flushbar/flushbar.dart';
 import 'bloc/membership_bloc.dart';
 import 'bloc/membership_event.dart';
 import 'bloc/membership_state.dart';
@@ -25,12 +24,15 @@ class MembershipDialog extends StatefulWidget {
 class _MembershipDialogState extends State<MembershipDialog> {
   final DialogStateHelper dialogHelper = DialogStateHelper();
 
-  Widget getFieldsWidget(BuildContext context, String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) {
-    return dialogHelper.fieldsWidget(context, <Widget>[buttons(appId, oldAccessModel, member)],
+  Widget getFieldsWidget(BuildContext context, String appId,
+      AccessModel oldAccessModel, MemberPublicInfoModel member) {
+    return dialogHelper.fieldsWidget(
+        context, <Widget>[buttons(appId, oldAccessModel, member)],
         height: 200, width: 200);
   }
 
-  Widget buttons(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) {
+  Widget buttons(
+      String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) {
     var privilegeLevel;
     if (oldAccessModel != null) {
       privilegeLevel = oldAccessModel.privilegeLevel;
@@ -39,31 +41,37 @@ class _MembershipDialogState extends State<MembershipDialog> {
     }
     List<Widget> _buttons = [];
     if (privilegeLevel == BLOCKED_MEMBERSHIP) {
-      _buttons.add(
-          RaisedButton(onPressed: () => _askUnblock(appId, oldAccessModel, member, privilegeLevel), child: Text('Unblock member')));
+      _buttons.add(RaisedButton(
+          onPressed: () =>
+              _askUnblock(appId, oldAccessModel, member, privilegeLevel),
+          child: Text('Unblock member')));
     } else if (privilegeLevel != OWNER_PRIVILEGES) {
-      _buttons
-          .add(RaisedButton(onPressed: () => _askBlock(appId, oldAccessModel, member), child: Text('Block member')));
+      _buttons.add(RaisedButton(
+          onPressed: () => _askBlock(appId, oldAccessModel, member),
+          child: Text('Block member')));
     }
     if ((privilegeLevel >= NO_PRIVILEGE) &&
         (privilegeLevel < LEVEL2_PRIVILEGE)) {
       _buttons.add(RaisedButton(
-          onPressed: () => _askPromote(appId, oldAccessModel, member, privilegeLevel),
+          onPressed: () =>
+              _askPromote(appId, oldAccessModel, member, privilegeLevel),
           child: Text('Promote member')));
     }
     if ((privilegeLevel > NO_PRIVILEGE) &&
         (privilegeLevel <= LEVEL2_PRIVILEGE)) {
       _buttons.add(RaisedButton(
-          onPressed: () => _askDemote(appId, oldAccessModel, member, privilegeLevel),
+          onPressed: () =>
+              _askDemote(appId, oldAccessModel, member, privilegeLevel),
           child: Text('Demote member')));
     }
-    _buttons.add(
-        RaisedButton(onPressed: () => _askSendMessage(member), child: Text('Send message')));
+    _buttons.add(RaisedButton(
+        onPressed: () => _askSendMessage(member), child: Text('Send message')));
     return ListView(
         shrinkWrap: true, physics: ScrollPhysics(), children: _buttons);
   }
 
-  void _askBlock(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) {
+  void _askBlock(
+      String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) {
     DialogStatefulWidgetHelper.openIt(
         context,
         YesNoDialog(
@@ -76,12 +84,14 @@ class _MembershipDialogState extends State<MembershipDialog> {
             noFunction: () => Navigator.pop(context)));
   }
 
-  void _askPromote(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) {
+  void _askPromote(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) {
     DialogStatefulWidgetHelper.openIt(
         context,
         YesNoDialog(
             title: 'Promote',
-            message: 'Do you want to promote this member? Current level is ' + privilegeLevelToString(privilegeLevel),
+            message: 'Do you want to promote this member? Current level is ' +
+                privilegeLevelToString(privilegeLevel),
             yesFunction: () {
               _promote(appId, oldAccessModel, member, privilegeLevel);
               Navigator.pop(context);
@@ -89,12 +99,14 @@ class _MembershipDialogState extends State<MembershipDialog> {
             noFunction: () => Navigator.pop(context)));
   }
 
-  Future<void> _askDemote(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) async {
+  Future<void> _askDemote(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) async {
     DialogStatefulWidgetHelper.openIt(
         context,
         YesNoDialog(
             title: 'Promote',
-            message: 'Do you want to demote this member? Current level is ' + privilegeLevelToString(privilegeLevel),
+            message: 'Do you want to demote this member? Current level is ' +
+                privilegeLevelToString(privilegeLevel),
             yesFunction: () {
               _demote(appId, oldAccessModel, member, privilegeLevel);
               Navigator.pop(context);
@@ -102,7 +114,8 @@ class _MembershipDialogState extends State<MembershipDialog> {
             noFunction: () => Navigator.pop(context)));
   }
 
-  void _askUnblock(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) {
+  void _askUnblock(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) {
     DialogStatefulWidgetHelper.openIt(
         context,
         YesNoDialog(
@@ -117,8 +130,8 @@ class _MembershipDialogState extends State<MembershipDialog> {
 
   void _askSendMessage(MemberPublicInfoModel member) {
     DialogStatefulWidgetHelper.openIt(
-        context,
-        RequestValueDialog(
+      context,
+      RequestValueDialog(
           title: 'Send Message to Member',
           yesButtonText: 'Send message',
           noButtonText: 'Discard',
@@ -127,52 +140,66 @@ class _MembershipDialogState extends State<MembershipDialog> {
             _sendMessage(msg, member);
             Navigator.pop(context);
           },
-          noFunction: () => Navigator.pop(context),
-        ));
+          noFunction: () => Navigator.pop(context)),
+    );
   }
 
-  Future<void> _block(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member) async {
+  Future<void> _block(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member) async {
     context.bloc<MembershipBloc>().add(BlockMember());
   }
 
-  Future<void> _promote(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) async {
+  Future<void> _promote(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) async {
     context.bloc<MembershipBloc>().add(PromoteMember());
   }
 
-  Future<void> _demote(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) async {
+  Future<void> _demote(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) async {
     context.bloc<MembershipBloc>().add(DemoteMember());
   }
 
-  Future<void> _unblock(String appId, AccessModel oldAccessModel, MemberPublicInfoModel member, int privilegeLevel) async {
+  Future<void> _unblock(String appId, AccessModel oldAccessModel,
+      MemberPublicInfoModel member, int privilegeLevel) async {
     context.bloc<MembershipBloc>().add(UnblockMember());
   }
 
-  void _sendMessage(String message, MemberPublicInfoModel member,) {
-    AbstractNotificationPlatform.platform
-        .sendMessage(context, member.documentID, message);
-    DialogStatefulWidgetHelper.openIt(
-        context, MessageDialog(title: 'Message', message: 'Message sent'));
+  void _sendMessage(
+    String message,
+    MemberPublicInfoModel member,
+  ) {
+    AbstractNotificationPlatform.platform.sendMessage(
+        context, member.documentID, message,
+        postSendAction: (value) {
+          Flushbar(
+            title:  "Message",
+            message:  "Yay! Message sent!",
+            duration:  Duration(seconds: 3),
+          )..show(context);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MembershipBloc, MembershipState>(
         builder: (context, state) {
-          if (state is MembershipLoaded) {
-            return dialogHelper.build(
-                title: state.member.name + ' - ' +
-                    privilegeLevelToString(state.accessModel.privilegeLevel),
-                contents: getFieldsWidget(context, state.appId, state.accessModel, state.member),
-                buttons: <FlatButton>[
-                  FlatButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Close'))
-                ]);
-          } else {
-            return DelayedCircularProgressIndicator();
-          }
-        });
+      if (state is MembershipLoaded) {
+        return dialogHelper.build(
+            title: state.member.name +
+                ' - ' +
+                privilegeLevelToString(state.accessModel.privilegeLevel),
+            contents: getFieldsWidget(
+                context, state.appId, state.accessModel, state.member),
+            buttons: <FlatButton>[
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Close'))
+            ]);
+      } else {
+        return DelayedCircularProgressIndicator();
+      }
+    });
   }
 }
