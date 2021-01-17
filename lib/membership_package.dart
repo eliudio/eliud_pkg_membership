a73f76db2ea938df3fd28fd3d7e51da571b0e725
Eliud Berlin <info@eliud.io>
2021-01-17 22:11:00 +0000

    Subscribe to access and member to allow update state of app when blocking/unblocking or changing member data

--- lib/membership_package.dart
+++ lib/membership_package.dart
@@ -1,23 +1,61 @@
-import 'package:eliud_core/core/access/bloc/access_bloc.dart';
-import 'package:eliud_core/core/navigate/navigate_bloc.dart';
+import 'package:eliud_core/core/access/bloc/access_event.dart';
+import 'package:eliud_core/model/abstract_repository_singleton.dart' as corerepo;
 import 'package:eliud_core/model/access_model.dart';
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_model.dart';
+import 'package:eliud_core/package/package_with_subscription.dart';
+import 'package:eliud_core/tools/query/query_tools.dart';
 import 'package:eliud_pkg_membership/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_membership/model/component_registry.dart';
 import 'package:eliud_pkg_membership/tools/task/membership_task_entity.dart';
 import 'package:eliud_pkg_membership/tools/task/membership_task_model.dart';
-import 'package:flutter_bloc/src/bloc_provider.dart';
 import 'package:eliud_pkg_workflow/tools/task/task_model.dart';
-import 'package:eliud_core/package/package.dart';
 
 import 'model/repository_singleton.dart';
 
-abstract class MembershipPackage extends Package {
+abstract class MembershipPackage extends PackageWithSubscription {
   static final String MEMBER_HAS_NO_MEMBERSHIP_YET = 'MemberHasNoMembershipYet';
+  AccessModel stateAccesModel;
+
+  void _setState(AccessModel currentAccess, {MemberModel currentMember}) {
+    if (stateAccesModel != currentAccess) {
+      // force the member's screen to update when blocked state is different
+      var refresh = (stateAccesModel != null) && (currentAccess != null) && (stateAccesModel.blocked != currentAccess.blocked);
+      accessBloc.add(MemberUpdated(currentMember, refresh: refresh));
+      stateAccesModel = currentAccess;
+    }
+  }
 
   @override
-  BlocProvider createMainBloc(NavigatorBloc navigatorBloc, AccessBloc accessBloc) => null;
+  void resubscribe(AppModel app, MemberModel currentMember) {
+    var appId = app.documentID;
+    if (currentMember != null) {
+      subscription = corerepo.accessRepository(appId: appId).listen((list) {
+        if (list.isNotEmpty) {
+          _setState(list.first, currentMember: currentMember);
+        } else {
+          _setState(null, currentMember: currentMember);
+        }
+      }, eliudQuery: getAccessQuery(
+          appId, currentMember.documentID));
+    } else {
+      _setState(null);
+    }
+  }
+
+  void unsubscribe() {
+    super.unsubscribe();
+    _setState(null);
+  }
+
+  static EliudQuery getAccessQuery(String appId, String memberId) {
+    return EliudQuery(
+        theConditions: [EliudQueryCondition(
+            DocumentIdField(),
+            isEqualTo: memberId
+        )]
+    );
+  }
 
   @override
   Future<bool> isConditionOk(String packageCondition, AppModel app, MemberModel member, bool isOwner, bool isBlocked, PrivilegeLevel privilegeLevel) async {
