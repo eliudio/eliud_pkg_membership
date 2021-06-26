e3e9b50c9a66576b58e0a38b0873f75131805ff5
Eliud <58745656+eliudio@users.noreply.github.com>
2021-06-26 14:48:18 +0100

    Bugfixes

--- lib/model/membership_dashboard_firestore.dart
+++ lib/model/membership_dashboard_firestore.dart
@@ -61,10 +61,11 @@ class MembershipDashboardFirestore implements MembershipDashboardRepository {
       var doc = await collection.get();
       return await _populateDocPlus(doc);
     } on Exception catch(e) {
-      print("Error whilst retrieving MembershipDashboard with id $id");
-      print("Exceptoin: $e");
       if (onError != null) {
         onError(e);
+      } else {
+        print("Error whilst retrieving MembershipDashboard with id $id");
+        print("Exceptoin: $e");
       }
     };
   }
