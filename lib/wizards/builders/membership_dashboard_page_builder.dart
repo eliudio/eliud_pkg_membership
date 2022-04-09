3dc125dfd3a9d17c796a7589d84e2f7d96a6c357
Eliud <58745656+eliudio@users.noreply.github.com>
2022-04-09 10:51:56 +0100

    Move follow_request_dashboard_page_builder, invite, follwers and following to pkg_follow!

--- lib/wizards/builders/membership_dashboard_page_builder.dart
+++ lib/wizards/builders/membership_dashboard_page_builder.dart
@@ -11,8 +11,9 @@ import 'package:eliud_core/model/page_model.dart';
 import 'package:eliud_pkg_membership/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_membership/model/membership_dashboard_component.dart';
 import 'package:eliud_pkg_membership/model/membership_dashboard_model.dart';
+import 'package:eliud_core/core/wizards/builders/single_component_page_builder.dart';
 
-class MembershipDashboardPageBuilder extends PageBuilder {
+class MembershipDashboardPageBuilder extends SingleComponentPageBuilder {
   MembershipDashboardPageBuilder(
       String uniqueId,
       String pageId,
@@ -52,52 +53,4 @@ class MembershipDashboardPageBuilder extends PageBuilder {
         componentIdentifier: componentIdentifier,
         title: "App members");
   }
-
-
-  Future<PageModel> _setupPage(
-      {required String componentName,
-        required String componentIdentifier,
-        required String title}) async {
-    return await corerepo.AbstractRepositorySingleton.singleton
-        .pageRepository(app.documentID!)!
-        .add(_page(
-        componentName: componentName,
-        componentIdentifier: componentIdentifier,
-        title: title));
-  }
-
-  PageModel _page(
-      {required String componentName,
-        required String componentIdentifier,
-        required String title}) {
-    List<BodyComponentModel> components = [];
-    components.add(BodyComponentModel(
-        documentID: "1",
-        componentName: componentName,
-        componentId: constructDocumentId(uniqueId: uniqueId, documentId: componentIdentifier)));
-
-    return PageModel(
-        documentID: constructDocumentId(uniqueId: uniqueId, documentId: pageId),
-        appId: app.documentID!,
-        title: title,
-        drawer: leftDrawer,
-        endDrawer: rightDrawer,
-        appBar: theAppBar,
-        homeMenu: theHomeMenu,
-        layout: PageLayout.ListView,
-        conditions: StorageConditionsModel(
-          privilegeLevelRequired:
-          PrivilegeLevelRequiredSimple.Level1PrivilegeRequiredSimple,
-        ),
-        bodyComponents: components);
-  }
-
-  Future<PageModel> doIt(
-      {required String componentName,
-        required String componentIdentifier,
-        required String title}) async {
-    return await _setupPage(
-        componentName: componentName,
-        componentIdentifier: componentIdentifier,
-        title: title);
-  }}
+}
