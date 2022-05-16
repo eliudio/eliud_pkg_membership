aa3e97a3ed7af8ec09958ca4cfeb5598d3b83418
Eliud <58745656+eliudio@users.noreply.github.com>
2022-05-16 18:32:15 +0100

    PopupMenuButton in style api

--- lib/editors/membership_dashboard_component_editor.dart
+++ lib/editors/membership_dashboard_component_editor.dart
@@ -213,17 +213,19 @@ class _MembershipDashboardComponentEditorState
                     widget.app,
                     title: text(widget.app, context,
                         (value.text ?? '?') + ' ' + (value.description ?? '?')),
-                    trailing: PopupMenuButton<int>(
+                    trailing: popupMenuButton<int>(
+                        widget.app, context,
                         child: Icon(Icons.more_vert),
-                        elevation: 10,
                         itemBuilder: (context) => [
-                              PopupMenuItem(
+                              popupMenuItem(
+                                widget.app, context,
                                 value: 1,
-                                child: text(widget.app, context, 'Update'),
+                                label: 'Update',
                               ),
-                              PopupMenuItem(
+                              popupMenuItem(
+                                widget.app, context,
                                 value: 2,
-                                child: text(widget.app, context, 'Delete'),
+                                label: 'Delete',
                               ),
                             ],
                         onSelected: (selectedValue) {
